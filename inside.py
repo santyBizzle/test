7f31ad59d06fda4ef4b4f8f3dc99c844f1281ad4
byronSanty <bscabezas@uce.edu.ec>
2020-10-01 09:50:35 -0500

    save

---
+++ inside.py
@@ -0,0 +1,21 @@
+print('Usuario Correspondiente')
+class Person:
+    pass
+jorge =  Person()
+
+class Person:
+    name = ''
+    school = ''
+    city = ''
+    year = ''
+    
+jorge = Person()
+jorge.name = 'Jorge'
+jorge.school = 'Mejia'
+jorge.city = 'Quito'
+jorge.year = '2020'
+
+print('Person - name >>',jorge.name)
+print('Person - school >>',jorge.school)
+print('Person - city >>',jorge.city)
+print('Person - year >>',jorge.year)
